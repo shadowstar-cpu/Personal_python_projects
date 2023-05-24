889a6392ab8496a1ef41e066e7852009c826d7d3
Ronald Nickelson <114312153+shadowstar-cpu@users.noreply.github.com>
2023-05-24 15:07:22 +0000

    fixing errors from pylint

--- jwlsad.py
+++ jwlsad.py
@@ -1,16 +1,17 @@
 import random
-def Dice_roll():
-    Max = int(input("What would you like to roll out of?\n"))
-    Number_of_dice = int(input("How many dice would you like to roll?\n"))
+def dice_roll():
+    maxnumber = int(input("What would you like to roll out of?\n"))
+    number_of_dice = int(input("How many dice would you like to roll?\n"))
     i = 1
-    while i <= Number_of_dice:
-        Roll = random.randint(1, Max)
-        print(Roll)
+    while i <= number_of_dice:
+        rolls = []
+        rolls.append(random.randint(1, maxnumber))
         i += 1
-Dice_roll()
+dice_roll()
 
 class Player:
-    def __init__(self, name, level, attack, intelligence, battle_intelligence, max_health, defense, speed, accuracy, stealth, action_number, bonus_action_number, max_encumbrance,):
+    def __init__(self, name, level, attack, intelligence, battle_intelligence, max_health,
+            defense, speed, accuracy, stealth,action_number, bonus_action_number, max_encumbrance,):
         self.name = name
         self.level = level
         self.attack = attack
@@ -30,45 +31,45 @@ class Player:
         self.weapons = []
         self.attack_type = self.weapons[0].damage_type
         self.armor = []
-        self.moves = []
+        self.movelist = []
         self.skills = []
         self.relations = {}
         self.init_attacks()
-    
+
     def new_item(self, item):
         self.inventory.append(item)
-    
+
     def new_weapon(self, weapon):
         self.weapons.append(weapon)
 
     def new_armor(self, armor):
         self.armor.append(armor)
-        
+
     def entity_name(self):
         return self.name
-    
+
     def moves(self):
         return self.moves
-    
+
     def damaged(self, damage):
         self.health = max(self.health - damage, 0)
         print(f"{damage} damage done to {self.entity_name()}")
-    
-    def __str__(self):
-        return f"A {self.level} level player named {self.name}, with {self.health} health."        
 
-class Tutorial_Player(Player):
+    def __str__(self):
+        return f"A {self.level} level player named {self.name}, with {self.health} health."
+
+class TutorialPlayer(Player):
     def init_attacks(self):
         self.attacks = []
 
-class item:
-    pass
-    
-class weapon:
+class Item:
     pass
 
-class armor:
+class Weapon:
     pass
 
-class move:
+class Armor:
+    pass
+
+class Move:
     pass
